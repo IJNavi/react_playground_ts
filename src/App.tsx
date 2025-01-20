3954ce93dcf6ae43fd3818b37c3f060125cc4d0e
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-01-20 05:51:30 -0300

    Programa para testes de aprendizado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
-import Home from './pages/home/Home'
+import './App.css';
+import Login from './pages/login/Login';
 
 function App() {
-
   return (
     <>
-        <Home />
+      <Login />
     </>
   )
 }
