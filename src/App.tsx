50307aa731b37e259e033b75f215fb191dbb516a
Ivan Barbosa <ivanjulio.ij@gmail.com>
2025-01-22 14:06:35 -0300

    Coockbook REACT 13md

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
-import Login from './pages/login/Login';
+import './App.css'
+import ListarUsuarios from './components/usuarios/listarusuarios/ListarUsuarios'
 
 function App() {
+
   return (
     <>
-      <Login />
+      <ListarUsuarios />
     </>
   )
 }
